8c8216c0aed88956761e4cb97ede520892844c93
Taylor Ericson <116030067+tkericson@users.noreply.github.com>
2022-11-12 00:23:52 -0600

    Added playRound function

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let playerScore = 0;
 let computerScore = 0;
-let winner = null;
 
-console.log(getComputerChoice());
+let playerChoice = prompt("What is your choice? (rock, paper, or scissors)");   
+let computerChoice = getComputerChoice();
+playRound(playerChoice, computerChoice);
+
 
 function getComputerChoice() {
     let choice = Math.floor((Math.random() * 3) + 1);
@@ -10,9 +12,57 @@ function getComputerChoice() {
     if(choice === 1) {
         return "rock";
     } else if(choice === 2) {
-        return "paper";
+        return "paper"; 
     } else {
         return "scissors";
     }
 }   
 
+function playRound(playerChoice, computerChoice) {
+    let winner = null;
+    
+    if(playerChoice === computerChoice) {
+        return "tie";
+    } else {
+
+        if(playerChoice === "rock") {
+
+            if(computerChoice === "paper") {
+                winner = "computer";
+                console.log(`You lose! ${computerChoice} beats ${playerChoice}`);
+                computerScore += 1;
+            } else {
+                winner = "player";
+                console.log(`You win! ${playerChoice} beats ${computerChoice}`);
+                playerScore += 1;
+            }
+        }
+
+        if(playerChoice === "paper") {
+
+            if(computerChoice === "scissors") {
+                winner = "computer";
+                console.log(`You lose! ${computerChoice} beats ${playerChoice}`);
+                computerScore += 1;
+            } else {
+                winner = "player";
+                console.log(`You win! ${playerChoice} beats ${computerChoice}`);
+                playerScore += 1;
+            }
+        }
+
+        if(playerChoice === "scissors") {
+
+            if(computerChoice === "rock") {
+                winner = "computer";
+                console.log(`You lose! ${computerChoice} beats ${playerChoice}`);
+                computerScore += 1;
+            } else {
+                winner = "player";
+                console.log(`You win! ${playerChoice} beats ${computerChoice}`);
+                playerScore += 1;
+            }
+        }
+        return winner;
+    }
+}
