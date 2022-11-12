fd06ce4f788ff42906a55b23063246bbcbdc6a96
Taylor Ericson <116030067+tkericson@users.noreply.github.com>
2022-11-11 23:59:51 -0600

    Added getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("This worked");
+let playerScore = 0;
+let computerScore = 0;
+let winner = null;
+
+console.log(getComputerChoice());
+
+function getComputerChoice() {
+    let choice = Math.floor((Math.random() * 3) + 1);
+    
+    if(choice === 1) {
+        return "rock";
+    } else if(choice === 2) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}   
+
